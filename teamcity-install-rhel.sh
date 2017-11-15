c8082f059af0b41f5e546a41267b8a5533448976
stasisha <stasisha@users.noreply.github.com>
2017-11-16 00:48:25 +0200

    Update teamcity-install-rhel.sh

--- teamcity-install-rhel.sh
+++ teamcity-install-rhel.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
 
-read -p 'Would you like to use PostgreSQL? [y/n]: ' psql_answer
-read -p 'Would you like to use SWAP? [y/n]: ' swap_answer
-
-#install lib
-yum install java-openjdk wget -y
-if [ "$psql_answer" == 'y' ] || [ "$psql_answer" == 'Y'  ]; then
-  yum install https://download.postgresql.org/pub/repos/yum/9.6/redhat/rhel-7-x86_64/pgdg-redhat96-9.6-3.noarch.rpm -y
-  yum install postgresql96-server -y
-fi
-
-export JAVA_HOME=/usr/lib/jvm/jre-openjdk
-
-
-
-    psql_pass=`< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32}`
-
-  echo "Postgres password: $psql_pass"
+pass=`< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32}`
+echo "Postgres password: $pass"
