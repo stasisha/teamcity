f032f2d9b598a842e2326af2a9dace5f3dc28e60
stasisha <stasisha@users.noreply.github.com>
2017-11-15 20:14:58 +0200

    Update teamcity-install.sh

--- teamcity-install.sh
+++ teamcity-install.sh
@@ -47,19 +47,19 @@ if [ -e '/usr/bin/wget' ]; then
         bash teamcity-install-$type.sh $*
         exit
     else
-        echo "Error: vst-install-$type.sh download failed."
+        echo "Error: teamcity-install-$type.sh download failed."
         exit 1
     fi
 fi
 
 # Check curl
 if [ -e '/usr/bin/curl' ]; then
-    curl -O http://vestacp.com/pub/vst-install-$type.sh
+    curl -O https://raw.githubusercontent.com/stasisha/teamcity-centos/master/teamcity-install-$type.sh
     if [ "$?" -eq '0' ]; then
-        bash vst-install-$type.sh $*
+        bash teamcity-install-$type.sh $*
         exit
     else
-        echo "Error: vst-install-$type.sh download failed."
+        echo "Error: teamcity-install-$type.sh download failed."
         exit 1
     fi
 fi
