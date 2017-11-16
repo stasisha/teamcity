29be22b70b1c6ac321d38578dba60d64ef4a98b4
stasisha <stasisha@users.noreply.github.com>
2017-11-16 03:54:14 +0200

    Update teamcity-install.sh

--- teamcity-install.sh
+++ teamcity-install.sh
@@ -13,26 +13,6 @@ if [ "x$(id -u)" != 'x0' ]; then
     exit 1
 fi
 
-# Check admin user account
-if [ ! -z "$(grep ^admin: /etc/passwd)" ] && [ -z "$1" ]; then
-    echo "Error: user admin exists"
-    echo
-    echo 'Please remove admin user account before proceeding.'
-    echo 'If you want to do it automatically run installer with -f option:'
-    echo "Example: bash $0 --force"
-    exit 1
-fi
-
-# Check teamcity user account
-if [ ! -z "$(grep ^teamcity: /etc/group)" ] && [ -z "$1" ]; then
-    echo "Error: group admin exists"
-    echo
-    echo 'Please remove teamcity user account before proceeding.'
-    echo 'If you want to do it automatically run installer with -f option:'
-    echo "Example: bash $0 --force"
-    exit 1
-fi
-
 # Detect OS
 case $(head -n1 /etc/issue | cut -f 1 -d ' ') in
     Debian)     type="debian" ;;
